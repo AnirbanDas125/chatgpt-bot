38265347651ab825e3b898cdf4b7f8d50ca27c97
Anirban <ad653157@gmail.com>
2023-12-01 15:04:58 +0530

    something wrong with api code is done

--- src/main/java/com/anirban/chatgptbot/controllers/BotController.java
+++ src/main/java/com/anirban/chatgptbot/controllers/BotController.java
@@ -1,8 +1,26 @@
 package com.anirban.chatgptbot.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.anirban.chatgptbot.services.BotService;
+import com.theokanning.openai.completion.CompletionResult;
+
 @RestController
+@RequestMapping("/chat")
 public class BotController {
+	
+	@Autowired
+	private BotService service;
+	
+	@GetMapping("/get-chat/{prompt}")
+	public ResponseEntity<CompletionResult> getChat(@PathVariable String prompt){
+		
+		return service.getChat(prompt);
+	}
 
 }
--- src/main/java/com/anirban/chatgptbot/services/BotService.java
+++ src/main/java/com/anirban/chatgptbot/services/BotService.java
@@ -1,16 +1,35 @@
 package com.anirban.chatgptbot.services;
 
+
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.theokanning.openai.OpenAiHttpException;
+import com.theokanning.openai.completion.CompletionRequest;
 import com.theokanning.openai.completion.CompletionResult;
+import com.theokanning.openai.service.OpenAiService;
 
 @Service
 public class BotService {
 
 	public ResponseEntity<CompletionResult> getChat(String prompt) {
 		
-		return null;
+		try {
+		OpenAiService service = new OpenAiService("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");
+		CompletionRequest request = CompletionRequest.builder()
+				.prompt(prompt)
+				.model("text-davinci-003")
+				.echo(true)
+				.build();
+		
+		return new ResponseEntity<>(service.createCompletion(request),HttpStatus.OK);
+		}catch(OpenAiHttpException e) {
+		  e.printStackTrace();
+          throw new RuntimeException(e.getMessage(),e);		 
+		}
+		
 	}
 
 }
